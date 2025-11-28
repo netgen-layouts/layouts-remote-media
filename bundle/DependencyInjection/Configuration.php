0c059bec2f534d849638c68d9be6cb33a4a18a0a
Edi Modrić <edi.modric@gmail.com>
2025-11-28 21:07:08 +0100

    Fix issues with PHPStan

--- bundle/DependencyInjection/Configuration.php
+++ bundle/DependencyInjection/Configuration.php
@@ -4,25 +4,19 @@ declare(strict_types=1);
 
 namespace Netgen\Bundle\LayoutsRemoteMediaBundle\DependencyInjection;
 
-use Symfony\Component\Config\Definition\Builder\ArrayNodeDefinition;
 use Symfony\Component\Config\Definition\Builder\TreeBuilder;
 use Symfony\Component\Config\Definition\ConfigurationInterface;
 
 final class Configuration implements ConfigurationInterface
 {
+    /**
+     * @return \Symfony\Component\Config\Definition\Builder\TreeBuilder<'array'>
+     */
     public function getConfigTreeBuilder(): TreeBuilder
     {
         $treeBuilder = new TreeBuilder('netgen_layouts_remote_media');
-
         $rootNode = $treeBuilder->getRootNode();
-        $this->addCacheConfiguration($rootNode);
-        $this->addRootFolderConfiguration($rootNode);
 
-        return $treeBuilder;
-    }
-
-    private function addCacheConfiguration(ArrayNodeDefinition $rootNode): void
-    {
         $rootNode
             ->children()
                 ->arrayNode('cache')
@@ -38,17 +32,12 @@ final class Configuration implements ConfigurationInterface
                         ->end()
                     ->end()
                 ->end()
-            ->end();
-    }
-
-    private function addRootFolderConfiguration(ArrayNodeDefinition $rootNode): void
-    {
-        $rootNode
-            ->children()
-                ->scalarNode('root_folder')
+                ->stringNode('root_folder')
                     ->info('Root folder in Cloudinary for content browser (see README)')
                     ->defaultNull()
                 ->end()
             ->end();
+
+        return $treeBuilder;
     }
 }
