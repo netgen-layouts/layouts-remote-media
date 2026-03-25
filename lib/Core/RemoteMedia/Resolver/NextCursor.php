3a5430bbad01c16cd7d46a5809bcf85598f2a1ae
Samuel Raus <sam.r@netgen.io>
2026-03-25 11:15:55 +0100

    Change trim to mb_trim

--- lib/Core/RemoteMedia/Resolver/NextCursor.php
+++ lib/Core/RemoteMedia/Resolver/NextCursor.php
@@ -11,7 +11,7 @@ use RuntimeException;
 
 use function implode;
 use function str_replace;
-use function trim;
+use function mb_trim;
 
 final class NextCursor implements NextCursorResolverInterface
 {
@@ -56,7 +56,7 @@ final class NextCursor implements NextCursorResolverInterface
     {
         $forbiddenCharacters = ['{', '}', '(', ')', '/', '\\', '@'];
         foreach ($forbiddenCharacters as $char) {
-            $key = str_replace($char, '_', trim($key, $char));
+            $key = str_replace($char, '_', mb_trim($key, $char));
         }
 
         return $key;
